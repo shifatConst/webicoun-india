3aa3d8ad03aa9169a4dad4299997c48707fb2cb3
shifatreza <shifatreza44@gmail.com>
2021-05-16 19:21:17 +0530

    video player added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -5,7 +5,12 @@ const Blogs = () => {
     const [blogs, setBlogs] = useState([])
     const api = 'https://webicoun-backend-hajkn.run-ap-south1.goorm.io/blogcat'
     useEffect(() => {
-        fetch(api)
+        fetch(api , {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               }
+        })
         .then(res => res.json())
         .then(data => setBlogs(data.blogs));
     }, []);
--- src/components/WebinarVideos/WebinarVideos.js
+++ src/components/WebinarVideos/WebinarVideos.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import ReactPlayer from "react-player"
 const WebinarVideos = (props) => {
     const { webiurl } = props.webinar;
     return (
-        <div className="col-sm-3">
-            <iframe width="420" height="315"
-                src={webiurl}>
-            </iframe>
+        <div className="col-sm-6">
+            <ReactPlayer className="m-3 text-center"
+                url={webiurl}
+            />
         </div>
     );
 };
